eef597f8ec6501ebc9537317abb2f94a5ea85541
Shoaib Nadeem SK <shoaib.nadeem@cloudeq.com>
2023-09-12 16:45:18 +0530

    add vars directory

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,3 +1,3 @@
 def call(){
-    git branch: 'main', changelog: false, poll: false, url: 'https://github.com/Shoaibnadeemsk/TU-TF.git'
+    git branch: 'main', url: 'https://github.com/Shoaibnadeemsk/TU-TF.git'
 }
